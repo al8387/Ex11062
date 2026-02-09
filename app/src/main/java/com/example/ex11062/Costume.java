84297abd56c82ae741aff896501e27c885fb6b13
Adam <adamitsverygoodman@gmail.com>
2026-02-09 14:44:10 +0200

    finish visuals

---
+++ app/src/main/java/com/example/ex11062/Costume.java
@@ -0,0 +1,17 @@
+package com.example.ex11062;
+
+public class Costume {
+    private int image;
+    private String name;
+    private int level;
+    private String category;
+    private String[] accessories;
+
+    public Costume(int image, String name, int level, String category, String[] accessories) {
+        this.image = image;
+        this.name = name;
+        this.level = level;
+        this.category = category;
+        this.accessories = accessories;
+    }
+}
